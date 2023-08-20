c5859610c3df9a7757ad0d4bd621782c9ff61e49
indeya20 <indeya.rafeeq@gmail.com>
2023-08-20 22:14:22 +0200

    PageLoadTimeout - removed unused import. Second commit

--- newproject/src/WebDrivercommands/pageLoadTimeoutCommand.java
+++ newproject/src/WebDrivercommands/pageLoadTimeoutCommand.java
@@ -1,7 +1,7 @@
 package WebDrivercommands;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
